d73f92d445370cdee2d2029c42b59337f9d1e488
Martijn Boland <martijn@boland.org>
2017-11-24 15:25:54 +0100

    Cleaning after 2.0 upgrade

--- src/MinimalAspNetCore.Mvc/Startup.cs
+++ src/MinimalAspNetCore.Mvc/Startup.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace MinimalAspNetCore.Mvc
 {
@@ -20,10 +19,8 @@ namespace MinimalAspNetCore.Mvc
             services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
